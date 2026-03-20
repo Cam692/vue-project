de085f9a05055d5405bbe93610d69b8a53b83d35
Cam692 <1771975333@qq.com>
2026-03-21 00:06:07 +0800

    项目初始化

--- vue-project/src/router/index.ts
+++ vue-project/src/router/index.ts
@@ -1,23 +1,13 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+// 创建路由实例由createRouter实现
+import {createRouter,createWebHashHistory,createWebHistory} from 'vue-router'
 
+// 路由模式：
+// history模式使用createWebHistory()
+// hash模式使用createWebHsshHistory()
+// 参数是基础路由的路径，默认/
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-  ],
+    history:createWebHistory(import.meta.env.BASE_URL),
+    routes: []
 })
 
 export default router
